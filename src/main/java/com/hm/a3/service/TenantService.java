4babbac384cc3c87dbfebc9f6862e4c0a47a2203
Amir <amir.khameneh@cmas-systems.com>
2022-05-16 18:37:14 +0100

    jwt

--- src/main/java/com/hm/a3/service/TenantService.java
+++ src/main/java/com/hm/a3/service/TenantService.java
@@ -3,12 +3,13 @@ package com.hm.a3.service;
 import com.hm.a3.dao.TenantRepository;
 import com.hm.a3.model.entity.Tenant;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TenantService {
 
 	private final TenantRepository tenantRepository;
